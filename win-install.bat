d757086d4b81b7d51a8302a3abc3e4db279d6cef
MaksymilianA <none@cert.cx>
2020-11-24 16:30:50 +0100

    fix import DB for windows

--- win-install.bat
+++ win-install.bat
@@ -5,5 +5,6 @@ call win-CONFIGURE.bat
 docker build -t cifrex .
 docker run --name cifrexdb -p %IP_DB%:%PORT_DB%:3306 -e MYSQL_ROOT_PASSWORD=%DB_ROOT_PASS% -d mariadb:latest
 TIMEOUT 30
-mysql  --defaults-file=./config/mariadb/.my.cnf -u root < ./SRC/DATABASE.sql
+mysql --defaults-file=./config/mariadb/.my.cnf -u root < ./SRC/DATABASE.sql
+mysql --defaults-file=./config/mariadb/.my.cnf -u root cifrex_prod < ./DBDumps/latest.sql
 docker run --link cifrexdb --name "cifrexweb" -p %IP_HTTP%:%PORT_HTTP%:80 -p %IP_HTTP%:%PORT_HTTPS%:443 -d -it cifrex
